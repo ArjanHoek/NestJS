9fa9f016012f103d21c140175cf99aedff89da64
Arjan Hoek <132347615+ArjanHoek-Topicus@users.noreply.github.com>
2023-11-19 18:59:38 +0100

    add validators

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,6 +12,7 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import {
+  ApiBadRequestResponse,
   ApiCreatedResponse,
   ApiNotFoundResponse,
   ApiOkResponse,
@@ -45,6 +46,7 @@ export class UsersController {
   }
 
   @ApiCreatedResponse({ type: User })
+  @ApiBadRequestResponse()
   @Post()
   createUser(@Body() body: CreateUserDto): User {
     return this.usersService.createUser(body);
